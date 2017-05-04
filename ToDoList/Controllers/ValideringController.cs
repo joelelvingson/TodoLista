3396e8fb35fa64971e4aa9d3ec75bb2c88834ff9
Joel <Ricard@DESKTOP-NMICJKT>
2017-05-04 20:44:19 +0200

    Test av validering

---
+++ ToDoList/Controllers/ValideringController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ValideringController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public ValideringController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Validering
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Todo val)
+        {
+            var hej = new Todo
+            {
+                Name = val.Name
+            };
+            _context.Todos.Add(hej);
+            _context.SaveChanges();
+
+            return View("Svar", hej);
+        }
+    }
+}
